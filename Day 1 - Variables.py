86a20fe2c9638f3dcee5194bbfc0030afb054ca2
Aakash Manjrekar <aakashmanjrekar@gmail.com>
2021-10-23 23:01:42 +0530

    Add files via upload

---
+++ Day 1 - Variables.py
@@ -0,0 +1,30 @@
+# #Printing in Python
+# print("hello world!")
+# print("Day 1 - Python Print Function")
+
+# #String Manipulation
+# print("Line 1\nLine 2\nLine 3")
+
+# #String Concatenation
+# print("My name is" + " Aakash")
+
+# #input() function
+# print("Hello " + input("What is your name?") + "!")
+
+# #input() len() exercise
+# print(len(input("What is your name?")))
+
+# #variable exercise - swap variables
+# a = input("a:")
+# b = input("b:")
+# temp = a
+# a = b
+# b = temp
+# print("a = " + a)
+# print("b = " + b)
+
+# Day 1 Project - Band Name Generator 
+print("Welcome to the Band Name Generator!")
+city = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet_name)
